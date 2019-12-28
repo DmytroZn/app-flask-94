024722971d341bad3c440842fa8bd0d7fb04fb08
Dmytrozn <Dmytro.zn.gmail.com>
2019-12-28 19:42:38 +0200

    a

--- app.py
+++ app.py
@@ -104,13 +104,13 @@ def start():
         keys_sorted = ['amount', 'currency', 'shop_id', 'shop_order_id']
         sha = f'{amount}:{currency_dict[payment_currency]}:{shop_id}:{shop_order_id}{secretKey}'
         sign = hashlib.sha256(f'{sha}'.encode()).hexdigest()
-        models.Payment(**{
-            'currency' : payment_currency,
-            'amount' : amount_float,
-            'date_time' : datetime.datetime.now(),
-            'description' : description,
-            'identeficate' : f'{sign}_{datetime.datetime.now()}'
-        }).save()
+        # models.Payment(**{
+        #     'currency' : payment_currency,
+        #     'amount' : amount_float,
+        #     'date_time' : datetime.datetime.now(),
+        #     'description' : description,
+        #     'identeficate' : f'{sign}_{datetime.datetime.now()}'
+        # }).save()
         logging.info("Programm done with EUR")
         return render_template('index.html', 
                                 url=url, payment_currency=payment_currency,
@@ -137,13 +137,13 @@ def start():
         response = answer.json()
         url1 = response['data']['url'] 
         webbrowser.open(url1, new=2)
-        models.Payment(**{
-            'currency' : payment_currency,
-            'amount' : amount_float,
-            'date_time' : datetime.datetime.now(),
-            'description' : description,
-            'identeficate' : f'{sign}_{datetime.datetime.now()}'
-        }).save()
+        # models.Payment(**{
+        #     'currency' : payment_currency,
+        #     'amount' : amount_float,
+        #     'date_time' : datetime.datetime.now(),
+        #     'description' : description,
+        #     'identeficate' : f'{sign}_{datetime.datetime.now()}'
+        # }).save()
         logging.info("Programm done with USD")
         return render_template('index.html', payment_currency=payment_currency, url1=url1)
     elif payment_currency == 'RUB':
@@ -174,13 +174,13 @@ def start():
         m_historytm = response['data']['data']['m_historytm']
         referer = response['data']['data']['referer']
 
-        models.Payment(**{
-            'currency' : payment_currency,
-            'amount' : amount_float,
-            'date_time' : datetime.datetime.now(),
-            'description' : description,
-            'identeficate' : f'{sign}_{datetime.datetime.now()}'
-        }).save()
+        # models.Payment(**{
+        #     'currency' : payment_currency,
+        #     'amount' : amount_float,
+        #     'date_time' : datetime.datetime.now(),
+        #     'description' : description,
+        #     'identeficate' : f'{sign}_{datetime.datetime.now()}'
+        # }).save()
         logging.info("Programm done with RUB")
         return render_template('index.html', 
                             payment_currency=payment_currency, 
